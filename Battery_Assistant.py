bcf86c09413013c3f1b0f61292cab5e22da2f663
gsunil2502 <56781242+gsunil2502@users.noreply.github.com>
2020-09-11 15:07:29 +0530

    Updated Battery_Assitant.py
    Added code for low battery indication and also audio indication for both battery full and battery low

--- Battery_Assistant.py
+++ Battery_Assistant.py
@@ -1,4 +1,5 @@
-from tkinter import * 
+from playsound import playsound
+from tkinter import *
 from tkinter.ttk import *
 import psutil
 import time
@@ -12,13 +13,14 @@ def display(percent, plugged):
     master = Tk() 
 
     # adding title to the tkinter window
-    master.title("Surya's Battery Assistant")
-
+    master.title(" Battery Assistant ")
+    x_coordinate = 460
+    y_coordinate = 300  
     # setting geometry of tk window 
-    master.geometry("300x150") 
+    master.geometry("%dx%d+%d+%d" %(300,100,x_coordinate,y_coordinate)) 
 
     # label for the text below    
-    l1 = Label(master, text = "Please Disconnect Charger") 
+    l1 = Label(master, text = "Sunil, please disconnect the charger") 
     l1.config( anchor = CENTER) 
     l1.pack()
     
@@ -29,7 +31,7 @@ def display(percent, plugged):
 
     # Button which when clicked on destroys the loop
     b2 = Button(master, text = "OK", command = master.destroy) 
-    b2.place(relx = 0.5, rely = 0.5, anchor = CENTER) 
+    b2.place(relx = 0.5, rely = 0.7, anchor = CENTER) 
 
     # infinite loop which is required to 
     # run tkinter program infinitely 
@@ -53,8 +55,21 @@ while(True):
 
         # if plugged in and battery is >= 95 display the message 
         if plugged == "Plugged In":
+            # plays mp3 file
+            playsound("C:\\Users\\Sunil\\Desktop\\Battery Assistant\\disconnect.mp3")      
             display(percent, plugged)
-
             # sleep for 5 minutes and loop back again
             time.sleep(seconds)
     # if battery percent is not >= 95 loops back and checks again
+    if percent <= 20:
+        # gets the plugged status from battery
+        plugged = battery.power_plugged 
+
+        plugged = "Plugged In" if plugged else "Not Plugged In"
+        
+        # if not plugged and battery is <=20 display the message 
+        if plugged == "Not Plugged In":
+            playsound("C:\\Users\\Sunil\\Desktop\\Battery Assistant\\connect.mp3")
+            display(percent, plugged)
+            # sleep for 5 minutes and loop back again
+            time.sleep(seconds)
