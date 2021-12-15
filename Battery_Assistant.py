4671979c36c7880be9a889681d6a8904b2d589d5
Jayasurya <jsuryakt@gmail.com>
2021-12-15 14:06:03 +0530

    Remove playsound and add pyttsx3 and also check for none on percentage

--- Battery_Assistant.py
+++ Battery_Assistant.py
@@ -1,8 +1,9 @@
 # Note if any of the below packages is not installed in your system then install it
 # Syntax "pip install <packagename>" where <packagename> can be playsound, psutil, time
-# open command prompt type command "pip install playsound"
+# open command prompt type command "pip install pyttsx3"
 # open command prompt type command "pip install psutil"
-from playsound import playsound
+# from playsound import playsound
+import pyttsx3
 from tkinter import *
 from tkinter.ttk import *
 import psutil
@@ -11,6 +12,7 @@ import time
 # for sleep 
 minutes = 5
 seconds = minutes * 60
+engine = pyttsx3.init()
 
 # creating Tk window 
 def display(percent, plugged):
@@ -24,7 +26,7 @@ def display(percent, plugged):
     master.geometry("%dx%d+%d+%d" %(300,100,x_coordinate,y_coordinate)) 
 
     # label for the text below    
-    l1 = Label(master, text = "Sunil, please disconnect the charger") 
+    l1 = Label(master, text = "Please disconnect the charger") 
     l1.config( anchor = CENTER) 
     l1.pack()
     
@@ -46,40 +48,47 @@ while(True):
 
     # gets the battery status
     battery = psutil.sensors_battery()
-    
-    # unpacks battery percent 
-    percent = battery.percent
 
-    if percent >= 95:
+    if battery:
+        # unpacks battery percentage
+        percent = battery.percent
+        # engine.say(f'System is at {percent} percent battery level')
+        # engine.runAndWait()
 
+        if percent >= 95:
         # gets the plugged status from battery
-        plugged = battery.power_plugged 
+            plugged = battery.power_plugged 
 
-        plugged = "Plugged In" if plugged else "Not Plugged In"
+            plugged = "Plugged In" if plugged else "Not Plugged In"
 
-        # if plugged in and battery is >= 95 display the message 
-        if plugged == "Plugged In":
-            # plays mp3 file
-            # Makesure of your pathname where the audio file was created
-            playsound("C:\\Users\\Sunil\\Desktop\\Battery Assistant\\disconnect.mp3")      
-            display(percent, plugged)
-            # sleep for 5 minutes and loop back again
-            time.sleep(seconds)
+            # if plugged in and battery is >= 95 display the message 
+            if plugged == "Plugged In":      
+                display(percent, plugged)
+                engine.say(f'System is at {percent} percent battery level')
+                engine.say(f'Please disconnect the charger')
+                engine.runAndWait()
+                # sleep for 5 minutes and loop back again
+                time.sleep(seconds)
+                
+            # if battery percent is not >= 95, checks for the below condition.
+        if percent <= 20:
+            # gets the plugged status from battery
+            plugged = battery.power_plugged 
+
+            plugged = "Plugged In" if plugged else "Not Plugged In"
             
-    # if battery percent is not >= 95, checks for the below condition.
-    if percent <= 20:
-        # gets the plugged status from battery
-        plugged = battery.power_plugged 
+            # if not plugged and battery is <=20 display the message 
+            if plugged == "Not Plugged In":
+                engine.say(f'System is at {percent} percent battery level')
+                engine.say(f'Please connect the charger')
+                engine.runAndWait()
+                display(percent, plugged)
+                # sleep for 5 minutes and loop back again
+                time.sleep(seconds)
+                
+        # if battery percent is not <= 20 loops back and checks again
 
-        plugged = "Plugged In" if plugged else "Not Plugged In"
-        
-        # if not plugged and battery is <=20 display the message 
-        if plugged == "Not Plugged In":
-            # plays mp3 file
-            # Makesure of your pathname where the audio file was created
-            playsound("C:\\Users\\Sunil\\Desktop\\Battery Assistant\\connect.mp3")
-            display(percent, plugged)
-            # sleep for 5 minutes and loop back again
-            time.sleep(seconds)
-            
-    # if battery percent is not <= 20 loops back and checks again
+    else:
+        engine.say("Cannot determine the battery level")
+        engine.runAndWait()
+        break
